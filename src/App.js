0a8cf46933628096fcce4419e1f911c59126b48c
OsipciucIon19 <ion-osipciuc@mail.ru>
2024-04-22 17:42:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className={'container'}>
+      <h1>Bine ai venit pe pagina Qraise!</h1>
+      <p>Această pagină este în proces de dezvoltare. Ne cerem scuze pentru incomoditățile create.</p>
+      <p>Pentru a ne contacta, nu ezita să ne scrii pe poșta noastră electronică:</p>
+      <p><a href="mailto:info@qraise.app">info@qraise.app</a></p>
+    </div>
+  );
+}
+
+export default App;
