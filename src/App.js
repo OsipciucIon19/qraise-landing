f2bde068006c44446b6bd32740711f5be50635b7
OsipciucIon19 <ion-osipciuc@mail.ru>
2024-04-23 13:11:56 +0200

    update colors and add logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from './assets/images/logo.png';
 
 function App() {
   return (
     <div className={'container'}>
-      <h1>Bine ai venit pe pagina Qraise!</h1>
+      <img width={119} src={logo} />
+      <h1>Bine ai venit pe pagina QRaise!</h1>
       <p>Această pagină este în proces de dezvoltare. Ne cerem scuze pentru incomoditățile create.</p>
       <p>Pentru a ne contacta, nu ezita să ne scrii pe poșta noastră electronică:</p>
       <p><a href="mailto:info@qraise.app">info@qraise.app</a></p>
